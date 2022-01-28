487042494a23e59121fe9a55a6b3eabe0c624973
Jaroslaw Roman Cudzich <internet.jarek@gmail.com>
2022-01-28 18:55:47 +0000

    Challenge 11 and 12.  Allowed DockingStation instances to be able to dock a Bike instance and to be able to read from an instance variable in order to returna bike which has been docked.  Raised an exception when try to #release_bike from the DockingStation instance when @dock_bikes is empty.

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,13 +3,23 @@ class DockingStation
   def initialize
     @dock_bikes = []
   end
+
   def release_bike
+    if empty?
+      raise "The docking station is empty."
+    end
     bike = Bike.new
   end
+  
   def dock_bike(bike)
     @dock_bikes << bike
-    bike
   end
+
+  def empty?
+    @dock_bikes.length == 0
+  end
+
+
   # @dock_bike
   # def dock_bike_num
   #   @dock_bikes.length
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,35 +1,47 @@
 require './lib/docking_station'
+require './lib/bike'
 describe DockingStation do 
-    it "responds to release_bike" do
-      docking_station = DockingStation.new
-      expect(docking_station).to respond_to(:release_bike)
-    end
-    it "responds to release_bike with a working? bike" do
-      docking_station = DockingStation.new
-      bike = docking_station.release_bike
-      expect(bike).to respond_to(:working?)
-    end
+  describe "#dock_bike" do
     it "responds to docking a bike" do
       docking_station = DockingStation.new
       expect(docking_station).to respond_to(:dock_bike)
     end
-    #it "responds to see if a bike has been docked - ie if there are bikes docked" do
-    #end  
     it "responds to a docking with one argument" do
       docking_station = DockingStation.new
-      expect(docking_station).to respond_to(:dock_bike).with(1).argument
+      expect(docking_station).to respond_to(:dock_bike)
     end  
-    it "responds to the passing of a new bike into method dock_bike" do
-      docking_station = DockingStation.new
-      bike = Bike.new
-      expect(docking_station.dock_bike(bike)).to eq bike
-    end
-    it "responds to checking that a bike has been docked" do
-      docking_station = DockingStation.new
-      bike = Bike.new
-      docking_station.dock_bike(bike)
-      expect (:dock_bikes.length > 0 == true)
-      
-    end
+    # it "responds to the passing of a new bike into method dock_bike" do
+    #   docking_station = DockingStation.new
+    #   bike = Bike.new
+    #   expect(docking_station.dock_bike(bike)).to eq bike
+    # end
+  end
 
+  describe "#release_bike" do
+
+    it "responds to release_bike" do
+      docking_station = DockingStation.new
+      expect(docking_station).to respond_to(:release_bike)
+    end
+    # it "responds to release_bike with a working? bike" do
+    #   docking_station = DockingStation.new
+    #   bike = docking_station.release_bike
+    #   expect(bike).to respond_to(:working?)
+    # end
+    
+    #it "responds to see if a bike has been docked - ie if there are bikes docked" do
+    #end  
+    
+    
+    # it "responds to checking that a bike has been docked" do
+    #   docking_station = DockingStation.new
+    #   docking_station.dock_bike(Bike)
+    #   expect (:dock_bikes.length > 0 == true)
+    # end
+    it "responds to checking that the docking station is empty when releasing a bike.  If so then an error is raised" do
+      docking_station = DockingStation.new
+      expect { docking_station.release_bike }.to raise_error("The docking station is empty.")
+      # expect {:dock_bikes.length == 0}.to raise_error("The docking station is empty.")
+    end
+  end
 end
